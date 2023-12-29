1b059a43cf75192a13cb52458d757c4a715faed6
Shubh Chaturvedi <chaturvedishubh650@gmail.com>
2023-12-29 20:01:03 +0530

    DEV: frontend for all messages

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,50 +3,29 @@
 
 import React from "react";
 import Message from "../components/Message";
+import InputEmoji from "react-input-emoji";
+import AudioMessage from "../components/AudioMessage";
+import CompressedMedia from "../components/CompressedMedia";
+import FileAttachment from "../components/FileAttachment";
 
 export default function Page() {
+  const [text, setText] = React.useState("");
   return (
     // Chat component
     <div className="w-full h-full flex text-center flex-col rounded-lg">
       <h2 className="m-5 text-2xl font-bold text-gray-800 text-white"> Scally Chat</h2>
       {/* chat area */}
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         {/* customer one chat box */}
         <div className="chat flex flex-col w-1/2 border border-bottom-gray-20 m-5">
               {/* customer name and profile heading */}
-              <div className="flex flex-row w-full">
+              <div className="flex flex-row w-full rounded-lg bg-gray-800">
                 {/* profile pic */}
-                <div className="rounded-l-lg p-2">
-                  <img src="https://avatars.githubusercontent.com/u/25126281?v=4" className="rounded-full w-10 h-10" />
+                <div className="
+                rounded-l-lg p-2
+                ">
+                <img width="32" height="32" src="https://img.icons8.com/windows/32/ffffff/left.png" alt="left"/>
                 </div>
-                <div className=" rounded-l-lg py-3 px-4">
-                  <h2 className="text-xl font-bold text-gray-800 text-white">Tashu Chaturvedi</h2>
-                </div>
-                
-              </div>
-          <div className="flex flex-col w-full h-full">
-            {/* chat box */}
-            <div className="flex flex-col w-full h-full overflow-y-scroll">
-              <Message />
-              <Message />
-              <Message />
-              </div>
-            </div>
-          {/* chat input */}
-          <div className="flex flex-row w-full">
-            <input
-            style={{ outline: "none" ,
-            border: "none"
-          }}
-            className="flex-grow rounded-l-lg p-2" placeholder="Type your message here..." />
-            <button className="border border-gray-20 rounded-r-lg py-2 px-5">Send</button>
-            </div>
-
-            </div>
-            <div className="chat flex flex-col w-1/2 border border-bottom-gray-20 m-5">
-              {/* customer name and profile heading */}
-              <div className="flex flex-row w-full">
-                {/* profile pic */}
                 <div className="rounded-l-lg p-2">
                   <img src="https://avatars.githubusercontent.com/u/25126281?v=4" className="rounded-full w-10 h-10" />
                 </div>
@@ -57,6 +36,49 @@ export default function Page() {
               </div>
           <div className="flex flex-col w-full h-full">
             {/* chat box */}
+            <div style={{
+              height: "500px"
+            }} className="flex flex-col w-full overflow-y-scroll">
+              <Message />
+              <FileAttachment />
+              <CompressedMedia />
+              <AudioMessage />
+              </div>
+            </div>
+          {/* chat input */}
+          <div className="flex flex-row w-full">
+            <button className="rounded-r-lg py-2 px-2"><img width="26" height="26" src="https://img.icons8.com/metro/30/ffffff/attach.png" alt="attach"/></button>
+
+            <InputEmoji
+      value={text} 
+      onChange={setText} 
+      cleanOnEnter 
+      placeholder="Type a message"
+    /> 
+            <button className="rounded-r-lg py-2 px-2">
+            <img width="30" height="30" src="https://img.icons8.com/dotty/80/ffffff/paper-plane.png" alt="paper-plane"/></button>
+            </div>
+
+            </div>
+            <div className="chat flex flex-col w-1/2 border border-bottom-gray-20 m-5">
+              {/* customer name and profile heading */}
+              <div className="flex flex-row w-full rounded-lg bg-gray-800">
+                {/* profile pic */}
+                <div className="
+                rounded-l-lg p-2
+                ">
+                <img width="32" height="32" src="https://img.icons8.com/windows/32/ffffff/left.png" alt="left"/>
+                </div>
+                <div className="rounded-l-lg p-2">
+                  <img src="https://avatars.githubusercontent.com/u/25126281?v=4" className="rounded-full w-10 h-10" />
+                </div>
+                <div className=" rounded-l-lg py-3 px-4">
+                  <h2 className="text-xl font-bold text-gray-800 text-white">Tanishq Soni</h2>
+                </div>
+                
+              </div>
+          <div className="flex flex-col w-full h-full">
+            {/* chat box */}
             <div className="flex flex-col w-full h-full overflow-y-scroll">
               <Message />
               <Message />
@@ -65,8 +87,16 @@ export default function Page() {
             </div>
           {/* chat input */}
           <div className="flex flex-row w-full">
-            <input className="flex-grow border border-gray-20 rounded-l-lg p-2" placeholder="Type your message here..." />
-            <button className="border border-gray-20 rounded-r-lg py-2 px-5">Send</button>
+            <button className="rounded-r-lg py-2 px-2"><img width="26" height="26" src="https://img.icons8.com/metro/30/ffffff/attach.png" alt="attach"/></button>
+
+            <InputEmoji
+      value={text} 
+      onChange={setText} 
+      cleanOnEnter 
+      placeholder="Type a message"
+    /> 
+            <button className="rounded-r-lg py-2 px-2">
+            <img width="30" height="30" src="https://img.icons8.com/dotty/80/ffffff/paper-plane.png" alt="paper-plane"/></button>
             </div>
 
             </div>
--- apps/web/components/Message.tsx
+++ apps/web/components/Message.tsx
@@ -8,10 +8,15 @@ export default function Message() {
    <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
       <div className="flex items-center space-x-2 rtl:space-x-reverse">
          <span className="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
-         <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
       </div>
-      <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">That's awesome. I think our users will really appreciate the improvements.</p>
-      <span className="text-sm font-normal text-gray-500 dark:text-green-400">Delivered</span>
+      
+      <p className="text-sm text-left font-normal py-2.5 text-gray-900 dark:text-white">That's awesome. I think our users will really appreciate the improvements.</p>
+      <div className="flex items-center 
+      justify-end
+      space-x-2 rtl:space-x-reverse">
+         <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
+         <span className="text-sm font-normal text-gray-500 dark:text-gray-400"><img width="20" height="20" src="https://img.icons8.com/fluency-systems-filled/20/40C057/double-tick.png" alt="double-tick"/></span>
+      </div>
    </div>
    <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" data-dropdown-placement="bottom-start" className="inline-flex self-center items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-600" type="button">
       <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
